07ccd22c3b017e8b930aedbe93297bfad19ea473
Rahul Sandhu <rahul@sandhuservices.com>
2022-07-08 12:42:34 +0100

    Add files via upload

--- arch-install.sh
+++ arch-install.sh
@@ -4,10 +4,8 @@ echo "Drive List:"
 lsblk
 
 read -p "Enter the drive to install to: " drivename
-read -p "Do you want to have a seperate home partition: (Y or N)" 
-hpartition
-declare -l hpartition
-hpartition=$hpartiton
+read -p "Do you want to have a seperate home partition: (Y or N)" hpartition
+hpartition="$(tr [A-Z] [a-z] <<< "$hpartition")"
 echo "$hpartition"
 read -p "Would you like to use systemd-boot or grub? " bootloader
 read -p "Enter your timezone: " timezone
@@ -17,5 +15,29 @@ read -p "Do you have an Intel or AMD CPU? " cpu
 read -p "Do you have NVIDIA graphics? " NVIDIA
 
 ### PARITIONING THE DRIVE
+case $hpartition in
+
+    yes)
+	read -p "Enter the size of your home partition (GB)" hsize
+	fdisk $drivename << FDISK_CMDS
+	g
+	n
+	1
+	
+	+500M
+	t
+	1
+	n
+	2
+	
+	+$hsizeG
+	n
+	3
+	
+	
+	w
+	q
+
+
 
 
